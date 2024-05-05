60f899a8ba6383f7fbe79f57ada0af85e5f7a946
Animesh Raghav <95857840+animeshraghav@users.noreply.github.com>
2024-05-05 09:44:25 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Vertical sum/vertical-sum.java
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			
+			    ArrayList <Integer> res = g.verticalSum(root);
+			    for (Integer num : res) System.out.print (num + " ");
+			    System.out.println();
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    void vSumUtil(Node root, int ind, Map<Integer, Integer> mp) {
+        if (root == null)
+            return;
+            
+        mp.put(ind, mp.getOrDefault(ind, 0) + root.data);
+        // call for left subtree
+        vSumUtil(root.left, ind-1, mp);
+        // call for right subtree
+        vSumUtil(root.right, ind+1, mp);
+    }
+    
+    public ArrayList <Integer> verticalSum(Node root) {
+        Map<Integer, Integer> mp = new TreeMap<>();
+        vSumUtil(root, 0, mp);
+        return new ArrayList<>(mp.values());
+    }
+}
